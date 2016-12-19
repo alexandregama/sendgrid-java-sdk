4b5123d37c3b3cc3a0ab2d7ee77a878ef9838407
Alexandre Gama <alexandre.gama.lima@gmail.com>
2016-12-19 09:48:49 -0200

    Treats a few kind of exceptions

--- src/main/java/com/sendgrid/internal/email/JerseyEmailApi.java
+++ src/main/java/com/sendgrid/internal/email/JerseyEmailApi.java
@@ -1,10 +1,14 @@
 package com.sendgrid.internal.email;
 
+import static javax.ws.rs.client.Entity.json;
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
+import static javax.ws.rs.core.Response.Status.UNAUTHORIZED;
+import static javax.ws.rs.core.Response.Status.Family.CLIENT_ERROR;
+import static javax.ws.rs.core.Response.Status.Family.SERVER_ERROR;
+
 import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
 
 import com.sendgrid.api.client.EmailApi;
 import com.sendgrid.api.client.SendGridApiKey;
@@ -15,7 +19,9 @@ import com.sendgrid.internal.exception.SendGridException;
 class JerseyEmailApi implements EmailApi {
 
 	private static final String SENDGRID_API_SEND_EMAIL = "https://api.sendgrid.com/v3/mail/send";
+
 	private static final String AUTHORIZATION = "Authorization";
+
 	private final SendGridApiKey key;
 
 	public JerseyEmailApi(SendGridApiKey key) {
@@ -27,17 +33,26 @@ class JerseyEmailApi implements EmailApi {
 		Response response = ClientBuilder
 			.newClient()
 			.target(SENDGRID_API_SEND_EMAIL)
-			.request(MediaType.APPLICATION_JSON)
+			.request(APPLICATION_JSON)
 			.header(AUTHORIZATION, "Bearer " + key.getApiKey())
-			.post(Entity.json(payload));
+			.post(json(payload));
 
-		if (response.getStatusInfo().equals(Status.UNAUTHORIZED)) {
+		if (response.getStatusInfo().equals(UNAUTHORIZED)) {
 			SendGridErrors errors = response.readEntity(SendGridErrors.class);
 			throw new SendGridException("Unauthorized user to send an email by SendGrid", errors);
 		}
-		if (response.getStatusInfo().equals(Status.BAD_REQUEST)) {
+		if (response.getStatusInfo().equals(BAD_REQUEST)) {
 			SendGridErrors errors = response.readEntity(SendGridErrors.class);
 			throw new SendGridException("Bad Request while trying to send an email by SendGrid", errors);
+		}
+		if (response.getStatusInfo().getFamily().equals(CLIENT_ERROR)) {
+			SendGridErrors errors = response.readEntity(SendGridErrors.class);
+			throw new SendGridException("An error ocurred while trying to send an email by SendGrid", errors);
+		}
+
+		if (response.getStatusInfo().getFamily().equals(SERVER_ERROR)) {
+			SendGridErrors errors = response.readEntity(SendGridErrors.class);
+			throw new SendGridException("An error ocurred in the server while trying to send an email by SendGrid", errors);
 		}
 	}
 
